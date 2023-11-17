398d0b9f89eec5b3c6ff443ee7ed9013f74d3953
kenziejbeats <mj0123827@yahoo.com>
2023-11-17 17:47:56 -0500

    Update of assignment 4

---
+++ Assignment4/Operations.java
@@ -0,0 +1,27 @@
+package Assignment4;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Operations {
+    public static void main(String[] args) {
+        
+        Set<String> set1 = new LinkedHashSet<>();
+        set1.add("George");
+        set1.add("Jim");
+        set1.add("John");
+        set1.add("Blake");
+        set1.add("Kevin");
+        set1.add("Michael");
+
+        Set<String> set2 = new LinkedHashSet<>();
+        set2.add("George");
+        set2.add("Kaitie");
+        set2.add("Kevin");
+        set2.add("Michelle");
+        set2.add("Ryan");
+
+        
+    }
+    
+}
