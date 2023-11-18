187441f86baeb9e64e82dc412ef0e27f30f5ab0d
kenziejbeats <mj0123827@yahoo.com>
2023-11-17 19:14:17 -0500

    Ass. 4 changes

--- Assignment4/FormatNumbers.java
+++ Assignment4/FormatNumbers.java
@@ -6,7 +6,7 @@ import java.text.ParseException;
 import java.util.Locale;
 
 public class FormatNumbers {
-    public static void main(String args[]){
+    public static void main(String[] args){
 
     double number = 12345.678;
     
